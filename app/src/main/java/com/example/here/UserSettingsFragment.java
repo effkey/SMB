445e20b236c76a104c3e3f9b6d8a4e0950a2b628
effkey <kubpi2019@gmail.com>
2023-03-26 20:48:53 +0200

    Dodanie fragmentu do zarządzania ustawieniami użytkownika, widok ustawień użytkownika.

---
+++ app/src/main/java/com/example/here/UserSettingsFragment.java
@@ -0,0 +1,23 @@
+package com.example.here;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UserSettingsFragment extends Fragment {
+
+    public UserSettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_settings, container, false);
+    }
+}
